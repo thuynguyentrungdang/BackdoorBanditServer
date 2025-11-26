7170b3f08a7006de5d8f2697683fc74bf0c95aac
Kobe Thuy <thuynguyentrungdang@gmail.com>
2025-11-26 14:40:12 +0700

    Bump version

--- BackdoorBanditServer/BackdoorBanditServer.cs
+++ BackdoorBanditServer/BackdoorBanditServer.cs
@@ -14,7 +14,7 @@ public record ModMetadata : AbstractModMetadata
     public override string ModGuid { get; init; } = "com.raitheraichu.backdoorbanditserver";
     public override string Name { get; init; } = "Backdoor Bandit Server";
     public override string Author { get; init; } = "RaiRaiTheRaichu";
-    public override SemanticVersioning.Version Version { get; init; } = new("2.0.1");
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.3");
     public override Range SptVersion { get; init; } = new("~4.0.0");
     public override string License { get; init; } = "MIT";
     public override bool? IsBundleMod { get; init; } = true;
